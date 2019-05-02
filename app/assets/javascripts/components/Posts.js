085b1a47e79b00c9e025eabe591075cff6eec660
aipanhara <titpanhara@gmail.com>
2019-05-02 19:46:15 +0700

    First Commit React On Rails

---
+++ app/assets/javascripts/components/Posts.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class Posts extends React.Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('/api/posts')
+      .then(response => {
+        this.setState({ posts: response.data.posts });
+      })
+  }
+
+  componentWillUnmount(){
+
+  }
+
+  renderAllPosts = () => {
+    return(
+      <ul>
+        {this.state.posts.map(post => (
+          <li key={post}>{post}</li>
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderAllPosts()}
+      </div>
+    )
+  }
+}
+
+export default Posts
